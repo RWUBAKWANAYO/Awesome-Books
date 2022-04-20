ac9fe4f2206b887263fd9c6e2562feea4309c215
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com.com>
2022-04-20 20:29:47 +0200

    Add functions to handle content display at navigation,Add time to the project

--- script.js
+++ script.js
@@ -79,3 +79,37 @@ list.addEventListener('click', () => {
   addBookSection.classList.add('display');
   contactSection.classList.add('display');
 });
+add.addEventListener('click', () => {
+  listSection.classList.add('display');
+  addBookSection.classList.remove('display');
+  contactSection.classList.add('display');
+});
+
+contact.addEventListener('click', () => {
+  listSection.classList.add('display');
+  addBookSection.classList.add('display');
+  contactSection.classList.remove('display');
+});
+
+// handle dates actions
+
+const handleTime = () => {
+  const dateContainer = document.querySelector('.date-container');
+  const dateFunc = new Date();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const year = dateFunc.getFullYear();
+  const month = months[dateFunc.getMonth()];
+  let currentDate = dateFunc.getDate();
+  const hour = dateFunc.getHours();
+  const minutes = dateFunc.getMinutes();
+  const second = dateFunc.getSeconds();
+  if (currentDate === 1) currentDate = 'first';
+  else if (currentDate === 2) currentDate = 'second';
+  else if (currentDate === 3) currentDate = 'third';
+  else currentDate += 'th';
+
+  const time = (hour < 12) ? `${month} ${currentDate} ${year}, ${hour} ${minutes} ${second} AM`
+    : `${month} ${currentDate} ${year}, ${hour} ${minutes} ${second} PM`;
+  dateContainer.innerHTML = time;
+};
+window.addEventListener('load', handleTime);
